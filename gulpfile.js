f8dd2fac76f91b901acb1a69e1ebc2dcf8078c56
nineya <361654768@qq.com>
2022-04-28 10:22:37 +0800

    build(gulp):支持对多级目录下的js文件递归打包

--- gulpfile.js
+++ gulpfile.js
@@ -16,119 +16,116 @@ const distPath = "./dist";
 const devModel = process.env.npm_config_dev;
 let releaseVersion = process.env.npm_config_release;
 
-if(devModel) {
-	releaseVersion = null
-	console.log('> 开发模式')
+if (devModel) {
+    releaseVersion = null
+    console.log('> 开发模式')
 }
 releaseVersion && console.log(`> 发布新版本：${releaseVersion}`)
 
 task("clean", () => {
-	return src([cssPath, jsPath, distPath], {
-		read: false,
-		allowEmpty: true,
-	}).pipe(
-		clean({
-			force: true,
-		})
-	);
+    return src([cssPath, jsPath, distPath], {
+        read: false,
+        allowEmpty: true,
+    }).pipe(
+        clean({
+            force: true,
+        })
+    );
 });
 
 task("css", () => {
-	const ignoreFiles = [].map((file) => `./src/css/${file}.less`);
+    const ignoreFiles = [].map((file) => `./src/css/${file}.less`);
 
-	let gw = src("./src/css/*.less", {
-		ignore: ignoreFiles,
-	})
-		.pipe(less())
-		.pipe(
-			autoprefix({
-				overrideBrowserslist: ["> 2%", "last 2 versions", "not ie 6-9"],
-				cascade: false,
-			})
-		);
-	if(!devModel) {
-		gw = gw.pipe(minifyCSS());
-	}
+    let gw = src("./src/css/*.less", {
+        ignore: ignoreFiles,
+    })
+        .pipe(less())
+        .pipe(
+            autoprefix({
+                overrideBrowserslist: ["> 2%", "last 2 versions", "not ie 6-9"],
+                cascade: false,
+            })
+        );
+    if (!devModel) {
+        gw = gw.pipe(minifyCSS());
+    }
 
-	return gw.pipe(
-	    rename({
+    return gw.pipe(
+        rename({
             suffix: ".min",
         })
     ).pipe(dest(cssPath));
 });
 
 task("version", (done) => {
-	if (releaseVersion == null) {
-		done();
-		return;
-	}
-	const themePath = "theme.yaml";
-	const packagePath = "package.json"
-	const themeData = fs.readFileSync(themePath, "utf8")
-		.replace(/^(([\s\S]*[\n\r]+)version:\s+)[^\s]+([\s\S]*)$/, "$1"+releaseVersion+"$3")
-	fs.writeFileSync(themePath, themeData);
-	let packageData = fs.readFileSync(packagePath, "utf8")
-		.replace(/\"version\":\s*\"[^\"]+\"/, `"version": "${releaseVersion}"`)
+    if (releaseVersion == null) {
+        done();
+        return;
+    }
+    const themePath = "theme.yaml";
+    const packagePath = "package.json"
+    const themeData = fs.readFileSync(themePath, "utf8")
+        .replace(/^(([\s\S]*[\n\r]+)version:\s+)[^\s]+([\s\S]*)$/, "$1" + releaseVersion + "$3")
+    fs.writeFileSync(themePath, themeData);
+    let packageData = fs.readFileSync(packagePath, "utf8")
+        .replace(/\"version\":\s*\"[^\"]+\"/, `"version": "${releaseVersion}"`)
     fs.writeFileSync(packagePath, packageData);
-	done()
+    done()
 });
 
 task("js", () => {
-	const getEntryData = () => {
-		const ignoreFiles = [];
+    const readFile = (prefix, dir, ignoreFiles) => {
+        let result = {};
+        let files = fs.readdirSync(dir, "utf-8");
+        files.forEach((file) => {
+            let filePath = path.join(dir, file)
+            let states = fs.statSync(filePath);
+            if (states.isDirectory()) {
+                Object.assign(result, readFile(path.join(prefix, file), filePath, ignoreFiles));
+            } else if (ignoreFiles.length
+                ? /\.js$/.test(file) && !ignoreFiles.includes(path.join(prefix, file))
+                : /\.js$/.test(file)) {
+                const fileName = file.replace(/.js$/, "");
+                result[path.join(prefix, fileName)] = resolve(filePath);
+            }
+        })
+        return result;
+    }
+    const getEntryData = () => {
+        return readFile("", "./src/js", [])
+    };
 
-		try {
-			let files = fs.readdirSync("./src/js", "utf-8");
-			files = files.filter((file) => {
-				return ignoreFiles.length
-					? /\.js$/.test(file) && !ignoreFiles.includes(file)
-					: /\.js$/.test(file);
-			});
-
-			const result = {};
-			files.forEach((file) => {
-				const fileName = file.replace(/.js$/, "");
-				result[fileName] = resolve(`./src/js/${file}`);
-			});
-			result.utils = resolve("./src/utils/utils.js");
-
-			return result;
-		} catch (error) {
-			throw new Error(err);
-		}
-	};
-
-	return webpack({
+    return webpack({
         mode: devModel ? "development" : "production",
-		entry: getEntryData(),
-		module: {
-			rules: [
-				{
-					test: /\.js$/,
-					loader: "babel-loader",
-					include: resolve("source"),
-					exclude: resolve("node_modules"),
-					options: {
-						presets: ["@babel/preset-env"],
-						plugins: ["@babel/plugin-transform-runtime"],
-					},
-				},
-			],
-		},
-		stats: "errors-only",
-		output: {
-			filename: "[name].min.js",
-		},
-	})
-		.pipe(uglify())
-		.pipe(dest(jsPath))
+        entry: getEntryData(),
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: "babel-loader",
+                    include: resolve("source"),
+                    exclude: resolve("node_modules"),
+                    options: {
+                        presets: ["@babel/preset-env"],
+                        plugins: ["@babel/plugin-transform-runtime"],
+                    },
+                },
+            ],
+        },
+        stats: "errors-only",
+        output: {
+            filename: "[name].min.js",
+        },
+    })
+        .pipe(uglify())
+        .pipe(dest(jsPath))
 });
 
 task("zip", () => {
-	const target = ['./source/**', './template/**', './*.ftl', './*.yaml', 'README.md', 'screenshot.png', 'LICENSE']
-	return src(target, { base: '.' })
-		.pipe(zip('halo-theme-dream.zip'))
-		.pipe(dest(distPath));
+    const target = ['./source/**', './template/**', './*.ftl', './*.yaml', 'README.md', 'screenshot.png', 'LICENSE']
+    return src(target, {base: '.'})
+        .pipe(zip('halo-theme-dream.zip'))
+        .pipe(dest(distPath));
 });
 
 task("default", series("clean", "version", parallel("css", "js"), "zip"));
