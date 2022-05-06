135395ecb162b8cbfa121ad5b3ce4dd0b8a0b82a
nineya <361654768@qq.com>
2022-05-06 10:56:40 +0800

    feat(custom&muisc):添加了音乐播放器自定义模块

--- src/js/mew-custom.js
+++ src/js/mew-custom.js
@@ -9,4 +9,33 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }
     );
+
+    customElements.define(
+        "mew-music",
+        class MewMusic extends HTMLElement {
+            constructor() {
+                super();
+                this.options = {
+                    id: this.getAttribute("id"),
+                    color: this.getAttribute("color") || "var(--theme)",
+                    autoplay: !!this.getAttribute("autoplay"),
+                };
+                if (!this.options.id) return (this.innerHTML = "网易云歌曲ID未填写！");
+                fetch(
+                    "https://api.i-meto.com/meting/api?server=netease&type=song&id=" +
+                    this.options.id
+                ).then(async (response) => {
+                    const audio = await response.json();
+                    new APlayer({
+                        container: this,
+                        lrcType: 3,
+                        theme: this.options.color,
+                        autoplay: this.options.autoplay,
+                        audio
+                    });
+                });
+            }
+
+        }
+    );
 });
