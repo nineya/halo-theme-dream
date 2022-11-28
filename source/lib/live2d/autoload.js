f551d4ec0c96ff8ccc3af1d5d741a17bf09094b4
nineya <lsw@nineya.com>
2022-11-29 02:57:01 +0800

    perf(live2d): 压缩看板娘样式文件

--- source/lib/live2d/autoload.js
+++ source/lib/live2d/autoload.js
@@ -1,5 +1,5 @@
 try {
-    ($("<link>").attr({href: "/themes/dream/source/lib/live2d/waifu.css", rel: "stylesheet", type: "text/css"}).appendTo('head'),
+    ($("<link>").attr({href: "/themes/dream/source/lib/live2d/waifu.min.css", rel: "stylesheet", type: "text/css"}).appendTo('head'),
     $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>'),
     $.ajax({url: "/themes/dream/source/lib/live2d/waifu-tips.min.js", dataType:"script", cache: true, success: function() {
             $.ajax({url: "/themes/dream/source/lib/live2d/live2d.min.js", dataType:"script", cache: true, success: function() {
